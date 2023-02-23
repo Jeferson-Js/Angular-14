6863db521b58b1f9dbc4c19171afd1698579ff41
Jeferson-Js <oliveira.jeferson.jsogmail.com>
2023-02-23 14:16:24 -0300

    Trabalhando com rotas angular 14

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { AppComponent } from './app.component';
+import { HomeComponentComponent } from './components/home-component/home-component.component';
+import { AboutComponentComponent } from './components/about-component/about-component.component';
+import { ContactComponentComponent } from './components/contact-component/contact-component.component';
+const routes: Routes = [
+  { path: '', component: AppComponent },
+  { path: 'home', component: HomeComponentComponent },
+  { path: 'about', component: AboutComponentComponent },
+  { path: 'contact', component: ContactComponentComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponentComponent } from './components/header-component/header-component.component';
+import { FooterComponentComponent } from './components/footer-component/footer-component.component';
+import { HomeComponentComponent } from './components/home-component/home-component.component';
+import { AboutComponentComponent } from './components/about-component/about-component.component';
+import { ContactComponentComponent } from './components/contact-component/contact-component.component';
+import { CardsComponentComponent } from './components/cards-component/cards-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponentComponent,
+    FooterComponentComponent,
+    HomeComponentComponent,
+    AboutComponentComponent,
+    ContactComponentComponent,
+    CardsComponentComponent
   ],
   imports: [
     BrowserModule,
